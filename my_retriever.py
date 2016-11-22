e9548c7c223bf9466af3b504ef038bd59be415cb
omorhefere <ooimoloame1@sheffield.ac.uk>
2016-11-22 21:50:23 +0000

    finished

--- my_retriever.py
+++ my_retriever.py
@@ -1,13 +1,12 @@
 import math
 import numpy as np
 
-
 class Retrieve:
     def __init__(self,index,termWeighting):
        
         self.index = index
         self.termWeighting = termWeighting
-    # get the total number of documents fro each collection
+    # get the total number of documents for each collection
     def size(self):
         invertedIndex = self.index
         documentCollection = set([])
@@ -15,16 +14,17 @@ class Retrieve:
             for key in invertedIndex[keys]:
                 documentCollection.add(key)
         return documentCollection
-        
+    #get the cosine similarity between two documents
     def similarity(self,query,document):
         docs = {}
-        
+       
         for key in document.keys():
             if key in query.keys():
+                #calculate the similarity
                 docs[key]=(query[key]/document[key])
            
         return sorted(docs, key=docs.get,reverse=True)
-        
+    #get the set of documents that have at least one query term 
     def candidateSet(self,query):
         candidateSet = set([])
         invertedIndex = self.index
@@ -32,8 +32,6 @@ class Retrieve:
         for k in query.keys():  
             # if query is in the inverted index
             if  k in invertedIndex:       
-                # the number of documents that contain the query term
-                documentFrequency = len(invertedIndex[k])        
                 termDocs = invertedIndex[k]
                 for key in termDocs:
                     #add the documents that have at least one query term to a set
@@ -43,77 +41,86 @@ class Retrieve:
     def forQuery(self,query):
         collectionSize = len(self.size()) 
         invertedIndex=self.index
+        # size of each document vector.
         docsToDocVec = {}
+        
 
         #set of documents containg at least on query term
         candidateSet = self.candidateSet(query)
         if self.termWeighting == 'binary':
-         
+            #dictionary that holds the size of each  vector
+            
             binaryXDocVec = {}
                                    
-            #find the size of each binary document vector           
+            #find the size of each binary document vector 
+         
             for doc in candidateSet:
-                binaryVecWeights = []
-                binaryWeights = []
+                binaryVecWeights = 0
+                binaryWeights = 0
                 
-                #find the size of each binary document vector
+                #find the binary vector for each query term and document that contains a query term
                 for k,v in invertedIndex.items():
+                    # if the query term is in the inverted index set its vector to 1 
                     if k in query.keys() and doc in v:           
-                        binaryWeights.append(1)
+                        binaryWeights +=1 # sum of binary vectors
                     if doc in v:
-                        binaryVecWeights.append(1**2)
-                docsToDocVec[doc]= np.sqrt(np.sum(binaryVecWeights))
-                binaryXDocVec[doc]=np.sum(binaryWeights)
+                        binaryVecWeights +=1 #sum of document vectors
+                # add word and document vectors to a dictionariy for their similarity to be calculated
+                docsToDocVec[doc]= np.sqrt(binaryVecWeights)
+                binaryXDocVec[doc]=binaryWeights
                         
-                
+                # get cosine similarity.
             return self.similarity(binaryXDocVec,docsToDocVec)
         elif self.termWeighting == 'tf':
+             #dictionary that holds the size of each  vector
              tfXDocVec = {}
+            
                             
-             #find the size of each binary document vector           
+                   
              for doc in candidateSet:
-                tfVecWeights = []
-                tfWeights = []
+                tfVecWeights = 0
+                tfWeights = 0
+                #find the term freqency vector for each query term and document that contains a query term    
                 for k in invertedIndex:
-                    
+                    #find the size of the document vector
                     if invertedIndex.get(k, False).get(doc, False) != False :
-                        tfVecWeights.append((invertedIndex.get(k).get(doc))**2)
+                        tfVecWeights += (invertedIndex.get(k).get(doc))**2 # sum of document vectors
                     if k in query.keys():
-                        if invertedIndex.get(k, False).get(doc, False) != False :
-                            if query[k] >1:
-                                tfWeights.append(query[k] * invertedIndex.get(k).get(doc) )
+                        if invertedIndex.get(k, False).get(doc, False) != False : # check if query term is in the index 
+                            if query[k] >1: # check if the number of occurences for the query term is greater than 1
+                                tfWeights += (query[k] * invertedIndex.get(k).get(doc) ) # sum the tf vectors
                             else:
-                                tfWeights.append(invertedIndex.get(k).get(doc) )
-                            
-                docsToDocVec[doc]= np.sqrt(np.sum(tfVecWeights))
-                tfXDocVec[doc]=np.sum(tfWeights)
+                                tfWeights += (invertedIndex.get(k).get(doc) )# sum the tf vectors
+                # add term and document vectors to a dictionariy for their similarity to be calculated          
+                docsToDocVec[doc]= np.sqrt(tfVecWeights)
+                tfXDocVec[doc]=tfWeights
 
-                
-                        
-                        
-                
+             # get cosine similarity.
              return self.similarity(tfXDocVec,docsToDocVec)
         else:
+             #dictionary that holds the size of each  vector
              tfidfXDocVec = {}
                             
              #find the size of each binary document vector           
              for doc in candidateSet:
-                tfidfVecWeights = []
-                tfidfWeights = []
+                tfidfVecWeights = 0
+                tfidfWeights = 0
+                #find the tfidf vector for each query term and document that contains a query term 
                 for k in invertedIndex:
-                    documentFrequency = len(invertedIndex[k])   
-                    idf_calculation = collectionSize/documentFrequency
+                    # calcualte the inverse document frequency score
+                    idf_calculation = collectionSize/len(invertedIndex[k]) 
                     idf = math.log(idf_calculation)
                     
                     if invertedIndex.get(k, False).get(doc, False) != False :
-                        tfidfVecWeights.append((invertedIndex.get(k).get(doc) *idf)**2)
+                        tfidfVecWeights +=( (invertedIndex.get(k).get(doc) *idf)**2)# sum of document vectors
                     if k in query.keys():
                         if invertedIndex.get(k, False).get(doc, False) != False :
                             if query[k] >1:
-                                tfidfWeights.append(query[k] * invertedIndex.get(k).get(doc) *idf )
+                                tfidfWeights += (query[k] * invertedIndex.get(k).get(doc) *idf ) # sum the tfidf vectors
                             else:
-                                tfidfWeights.append(invertedIndex.get(k).get(doc) *idf)
-                            
-                docsToDocVec[doc]= np.sqrt(np.sum(tfidfVecWeights))
-                tfidfXDocVec[doc]=np.sum(tfidfWeights)
+                                tfidfWeights += (invertedIndex.get(k).get(doc) *idf) # sum the tfidf vectors
+                # add tfidf and document vectors to a dictionariy for their similarity to be calculated  
+                docsToDocVec[doc]= np.sqrt(tfidfVecWeights)
+                tfidfXDocVec[doc]=tfidfWeights
+                # get cosine similarity.
              return self.similarity(tfidfXDocVec,docsToDocVec)
